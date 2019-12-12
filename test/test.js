e001e558cb7075d3eee94d363350c8e0f8150bd5
Ben Yang <ben60523@gmail.com>
2019-12-12 14:24:52 +0800

    Detect Wi-Fi dongle by driver name

--- test/test.js
+++ test/test.js
@@ -1,45 +1,13 @@
+'use-strict'
 WiFiControl = require("../lib/wifi-control.js");
+const sleep = (millis) => new Promise(resolve => setTimeout(resolve, millis));
+
 
 WiFiControl.init({
   debug: true,
   connectionTimeout: 2000
 });
 
-
-/*
- *  Get info about wireless interface!
- */
-console.log( WiFiControl.getIfaceState() );
-
-/*
- *  Scan for nearby WiFi!
- */
-WiFiControl.scanForWiFi( function(error, response) {
-  if (error) console.log(error);
-  console.log(response);
-});
-
-
-/*
- *  Connect to an Access Point!
- */
-var open_ap = {
-  ssid: "And We Will Call It....THIS LAN!"
-};
-var closed_ap = {
-  ssid: "And We Will Call It....THIS LAN!",
-  password: "hench4life"
-};
-
-WiFiControl.connectToAP( closed_ap, function(error, response) {
-  if (error) console.log(error);
-  console.log(response);
-});
-
-/*
- *  Reset the WiFi card!
- */
-WiFiControl.resetWiFi( function(error, response) {
-  if (error) console.log(error);
-  console.log(response);
-});
+setInterval(() => {
+	WiFiControl.getIfaceState()
+}, 1000)
