a34a723ece8c828216d1ebb15bcd0cfb6061294e
Ben Yang <ben60523@gmail.com>
2019-12-16 14:30:19 +0800

    Show all wifi states including usb adapter

--- test/test.js
+++ test/test.js
@@ -8,6 +8,20 @@ WiFiControl.init({
   connectionTimeout: 2000
 });
 
-setInterval(() => {
-	WiFiControl.getIfaceState()
-}, 1000)
+var a = WiFiControl.getIfaceState()
+console.log(a.find(interface => interface.adapterName === 'Wi-Fi'))
+
+a.forEach(state => {
+  if (state && state.ssid.startsWith('MediCam') && state.connection === 'connected') {
+    console.log(state)
+  }
+});
+
+// ap = {
+//   ssid: 'UNICORNWORKING-5G',
+//   password: '25175089'
+// }
+
+// WiFiControl.connectToAP(ap, a[0].adapterName, () => {
+//   console.log("RRRRRRRRRRRRRRRRRRRR")
+// })
