4aad88be1803a48d8c277d90f531782cec9bb465
fasmedo <fasmedo@fasmedode-MacBook-Pro.local>
2020-04-24 11:27:12 +0800

    Connect device with native adapter of PC

--- test/test.js
+++ test/test.js
@@ -5,29 +5,29 @@ const sleep = (millis) => new Promise(resolve => setTimeout(resolve, millis));
 
 WiFiControl.init({
     debug: true,
-    connectionTimeout: 2000
+    connectionTimeout: 7000
 });
 let iface = WiFiControl.getIfaceState()
 
 console.log("getInterfaceState", iface);
 
-// WiFiControl.resetWiFi(iface[0].adapterName, (err) => {
-//     if (err) {
-//         console.log(err)
-//     }
-// })
+WiFiControl.resetWiFi(iface[0].adapterName, (err) => {
+    if (err) {
+        console.log(err)
+    }
+})
 
-// let ap = { ssid: 'your_ap_ssid', password: 'your_ap_password' }
+let ap = { ssid: 'MediCam_DFBAD1', password: '1234567890' }
 
-// WiFiControl.connectToAP(ap, iface[0].adapterName, (err, resp) => {
-//     if (resp) {
-//         console.log("connected: ", resp)
-//         // WiFiControl.resetWiFi(iface[0].adapterName, (err) => {
-//         //     if (err) {
-//         //         console.log(err)
-//         //     }
-//         // })
-//     }
-//     if (err)
-//         console.log("error", err)
-// })
+WiFiControl.connectToAP(ap, iface[0].adapterName, (err, resp) => {
+    if (resp) {
+        console.log("connected: ", resp)
+        // WiFiControl.resetWiFi(iface[0].adapterName, (err) => {
+        //     if (err) {
+        //         console.log(err)
+        //     }
+        // })
+    }
+    if (err)
+        console.log("error", err)
+})
